0e47fec4cae234ccb8491b022d039dd8e58b6081
vyash5075 <vyash5075@gmail.com>
2020-02-06 21:38:53 +0530

    merging lists

---
+++ mergelists.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package array;
+import java.util.*;
+/**
+ *
+ * @author yash verma
+ */
+public class mergelists {
+   public static void main(String args[])
+   {
+       Scanner sc=new Scanner(System.in);
+       System.out.println("enter the no. of inputs");
+      int n=sc.nextInt();
+      sc.nextLine();
+      ArrayList<Integer>al=new ArrayList<>();
+      for(int i=0;i<n;i++)
+      {
+        String s=sc.nextLine();
+       String arr[]=s.split("\\s+");
+       
+       for(int j=0;j<arr.length;j++)
+       {
+           System.out.println(arr[j]);
+           al.add(Integer.parseInt(arr[j]));
+       }  
+      } 
+     Collections.sort(al);  
+    System.out.println(al);
+   }
+}
